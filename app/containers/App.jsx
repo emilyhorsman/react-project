11d4055d8424ba02289ae8aee80fdefcae374ccc
Emily Horsman <me@emilyhorsman.com>
2016-01-25 17:25:16 -0500

    Redux logging

---
+++ app/containers/App.jsx
@@ -0,0 +1,8 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+export default class App extends Component {
+  render() {
+    return <h1>Test</h1>;
+  }
+}
--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,26 @@
 import 'babel-core/polyfill'
 
 import React from 'react'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { render } from 'react-dom'
-import App from './components/App'
+import createLogger from 'redux-logger'
+import App from './containers/App'
+import root from './reducers'
 
+const logger = createLogger()
+const createStoreWithMiddleware = applyMiddleware(logger)(createStore)
+const store = createStoreWithMiddleware(root)
 
-const app = document.createElement('div');
-document.body.appendChild(app);
-render(<App name="Alice" />, app);
+const app = document.createElement('div')
+document.body.appendChild(app)
+
+setTimeout(() => {
+  store.dispatch({ type: 'FOO' })
+}, 1000)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, app)
