94f57e2be8bc5dc64fb12e8beed65a319b0ee155
Emily Horsman <me@emilyhorsman.com>
2016-01-26 19:00:43 -0500

    Add support for the redux-devtools-extension

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,7 @@
 import 'babel-core/polyfill'
 
 import React from 'react'
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
 import createLogger from 'redux-logger'
@@ -9,9 +9,11 @@ import createLogger from 'redux-logger'
 import App from './containers/App'
 import root from './reducers'
 
-const logger = createLogger()
-const createStoreWithMiddleware = applyMiddleware(logger)(createStore)
-const store = createStoreWithMiddleware(root)
+const createFinalStore = compose(
+  applyMiddleware(createLogger()),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore)
+const store = createFinalStore(root)
 
 render(
   <Provider store={store}>
